7d9841c9ea6c9794970d4e09f8207cce52c149e3
tobi <tobiemba@gmail.com>
2023-11-09 12:57:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+function valider() {
+    var nombre = parseInt(document.getElementById("nombreInput").value );
+    if (!isNaN(nombre)) {
+        localStorage.setItem("nombre", nombre);
+        window.location.href = "inputs/inputs.html";
+    }
+}
+
+// pour la page inputs
+var nombre = localStorage.getItem("nombre");
+
+        for (var i = 0; i < nombre; i++) {
+
+            var input = document.createElement("input");
+            input.classList.add("createInput");
+            input.type = "text";
+            input.name = "input" + i;
+            input.placeholder = "Entrer un nom ";
+            document.getElementById("inputsContainer").appendChild(input);
+
+        }
+
+
+  function SaveName() {
+    var inputs = document.querySelectorAll('input');
+    var contenuInputs = [];
+
+    inputs.forEach(function(input) {
+    contenuInputs.push(input.value);
+    });
+
+  function shuffleArray(array) {
+
+      var currentIndex = array.length,
+      temporaryValue, randomIndex;
+      
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1; 
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+
+     }
+
+      return array;
+    }
+  var values = shuffleArray(contenuInputs);
+  localStorage.setItem('names', JSON.stringify(values));
+  window.location.href = "../liste/liste.html";
+
+
+}
+
+
+
+        
+
---
+++ liste/liste.js
@@ -0,0 +1,32 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var listName = localStorage.getItem('names');
+    var listeNoms = JSON.parse(listName);
+    var nombre = localStorage.getItem('nombre');
+    
+    for (var i = 0; i < nombre; i++) {
+      var listContainer = document.getElementsByClassName("list_container")[0];
+      var input = document.createElement("input");
+      input.classList.add("createName");
+      input.value = listeNoms[i];
+      listContainer.appendChild(input);
+    }
+    
+    localStorage.clear();
+  });
+  
+// pour la numerotation
+
+  window.onload = function() {
+    var form = document.getElementById("myForm");
+    var inputs = form.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+      var span = document.createElement("span");
+      span.innerHTML = (i + 1);
+      inputs[i].parentNode.insertBefore(span, inputs[i]);
+      inputs[i].setAttribute("id", (i + 1));
+      span.classList.add("spanInput");
+    }
+  };
+  
